769a0089e169e181ce265ae853bdc4d42e09851c
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2021-09-27 21:55:54 +0900

    update server

---
+++ server/testsite/my_settings.py
@@ -0,0 +1,12 @@
+DATABASES={
+        'default':{
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME':'covid',
+            'USER':'root',
+            'PASSWORD':'0955',
+            'HOST':'localhost',
+            'PORT':'3307',
+        }
+
+}
+SECRET_KEY='django-insecure-qyb(pve9#k^b+3^8ow&=0lp(wvfm32^eanj1t^qe70t+c_s31)'
