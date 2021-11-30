31fedccce87e18a1aa15e5de7e2f56f90a124a12
Mython <swag0090@naver.com>
2021-12-01 00:06:14 +0900

    edit something
    d

--- covid_19_all/CovidPatientRoute.py
+++ covid_19_all/CovidPatientRoute.py
@@ -12,6 +12,8 @@ def GetPatientRoute():
             'http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=12&ncvContSeq=&contSeq=&board_id=&gubun=')
         soup = BeautifulSoup(url.content, "html.parser")
         route = soup.select("#content > div > div.box_line2 > div > div > table > tbody > tr")
+        print(route)
+
         for i in range(0, len(route), 1):
             route[i] = route[i].text.replace("소독완료", "").replace("\n", " ")[1:-2]
 
--- server/testsite/corona/urls.py
+++ server/testsite/corona/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.index, name='login'),
     path('news_page', views.news_page, name='login'),
     path('gnews_page', views.gnews_page, name='login'),
+    path('pandj_page',views.patientjenan_page,name='login'),
+
     #func_links
     path('news/<str:query>',views.get_news,name="news"),
     path('gnews/',views.get_gnews,name="gnews"),
@@ -13,6 +15,7 @@ urlpatterns = [
     path('gps/<str:user_lng>/<str:user_lat>', views.get_location, name="get_location"),
     path('jenan/<str:area>',views.jenan,name="jenan"),
     path('calendar/', views.CalendarView.as_view(), name='calendar'),
+    path('junsu/', views.junsu, name='login'),
 
     #
     url(r'^event/new/$', views.event, name='event_new'),
